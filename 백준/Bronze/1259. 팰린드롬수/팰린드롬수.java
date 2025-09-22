345309ce92ae90cdc775f17b10fa0cd2b54a3202
TaeHoon <taehoondev030@gmail.com>
2025-09-22 19:46:46 +0900

    [Bronze I] Title: 팰린드롬수, Time: 104 ms, Memory: 13976 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while(true) {
+            String str = br.readLine();
+            boolean check = true;
+
+            if(str.equals("0")) break;
+
+            for(int i = 0; i < str.length() / 2; i++) {
+                if(str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+                    check = false;
+                    break;
+                }
+            }
+
+            if(check == true) {
+                System.out.println("yes");
+            } else {
+                System.out.println("no");
+            }
+        }
+    }
+}
